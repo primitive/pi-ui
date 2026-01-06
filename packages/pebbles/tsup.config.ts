3901199f68c8eeb58d79542f45287b2debd76383
Shaun Knowles <shaun@primitiveweb.dev>
2026-01-06 11:13:12 +0000

    feat: update tsup configuration to include inline import plugin for CSS files

--- packages/pebbles/tsup.config.ts
+++ packages/pebbles/tsup.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "tsup";
+import inlineImport from "esbuild-plugin-inline-import";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
   outDir: "dist",
-  dts: true,
   clean: true,
-  sourcemap: true
+  dts: true,
+  sourcemap: true,
+  esbuildPlugins: [
+    inlineImport({
+      filter: /\.css$/
+    })
+  ]
 });
